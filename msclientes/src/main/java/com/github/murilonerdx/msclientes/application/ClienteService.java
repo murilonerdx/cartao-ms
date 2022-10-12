6b87c565e5a282bd7d40a3bbc8a4d2c3b14bce86
Murilo P.S <mu-silva@outlook.com>
2022-10-12 19:10:46 -0300

    Adicinando novo projeto

---
+++ msclientes/src/main/java/com/github/murilonerdx/msclientes/application/ClienteService.java
@@ -0,0 +1,26 @@
+package com.github.murilonerdx.msclientes.application;
+
+import com.github.murilonerdx.msclientes.domain.Cliente;
+import com.github.murilonerdx.msclientes.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
---
+++ msclientes/src/main/java/com/github/murilonerdx/msclientes/application/representation/ClienteSaveRequest.java
@@ -0,0 +1,15 @@
+package com.github.murilonerdx.msclientes.application.representation;
+
+import com.github.murilonerdx.msclientes.domain.Cliente;
+import lombok.Data;
+
+@Data
+public class ClienteSaveRequest {
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel(){
+        return new Cliente(cpf, nome, idade);
+    }
+}
